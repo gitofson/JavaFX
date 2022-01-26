2ce8d1f807f4d0d04456788de1c761701263affe
stemberk <stemberk@spsmb.cz>
2022-01-26 11:06:49 +0100

    BidirectionalBinding

---
+++ src/cz/spsmb/w21/BidirectionalBinding.java
@@ -0,0 +1,36 @@
+package cz.spsmb.w21;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class BidirectionalBinding {
+    public static void main(String[] args) {
+        IntegerProperty x = new SimpleIntegerProperty(1);
+        IntegerProperty y = new SimpleIntegerProperty(2);
+        IntegerProperty z = new SimpleIntegerProperty(3);
+
+        System.out.println("před bindováním: ");
+        System.out.println(" x="+x.get()+" y="+y.get()+" z="+z.get());
+
+        x.bindBidirectional(y);
+        System.out.println("po 1. bindování: ");
+        System.out.println(" x="+x.get()+" y="+y.get()+" z="+z.get());
+
+        x.bindBidirectional(z);
+        System.out.println("po 2. bindování: ");
+        System.out.println(" x="+x.get()+" y="+y.get()+" z="+z.get());
+
+        System.out.println("po změně z: ");
+        z.set(19);
+        System.out.println(" x="+x.get()+" y="+y.get()+" z="+z.get());
+
+        //odstranění bindování
+        x.unbindBidirectional(y);
+        x.bindBidirectional(z);
+        System.out.println("Po odbindování a změně všech vlastností (x,y,z)");
+        x.set(100);
+        y.set(200);
+        z.set(300);
+        System.out.println(" x="+x.get()+" y="+y.get()+" z="+z.get());
+    }
+}
