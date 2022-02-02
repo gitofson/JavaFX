cb3bbe2ec2ddff830e57a9419ef9efd1cc810165
stemberk <stemberk@spsmb.cz>
2022-02-02 15:23:12 +0100

    stages+

---
+++ src/cz/spsmb/w22/stages/BEverRunnigApp.java
@@ -0,0 +1,15 @@
+package cz.spsmb.w22.stages;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BEverRunnigApp extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Do not write any code here
+    }
+}
