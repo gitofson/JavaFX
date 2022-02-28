03230316d0eb82df05275bed54c199bbce1134f6
stemb <stemberk@spsmb.cz>
2022-02-28 19:42:28 +0100

    FCSSPropertiesTypes

---
+++ src/cz/spsmb/w25/style/FCSSPropertiesTypes.java
@@ -0,0 +1,28 @@
+package cz.spsmb.w25.style;
+// JavaFX CSS podporuje následující typy:
+// inherit      - umožňuje převzít hodnotu CSS vlastnosti z nadřazeného uzlu.
+// booelan      -
+// string       - může být uveden pomocí jednoduchých i dvojitých uvozovek.  Nejběžnější typ.
+// number       - celočíselný, či reálný typ. Je zde umožněno specifikovat i jednotku - px (pixely), mm (milimetry).
+//                cm (centimetry), in (palce), pt (body), pc (picas), em, ex, nebo %.
+// angle        - totéž jako number, jen jednotky mohou být deg (stupně), nebo rad (radiány).
+// point        - reprezentován 2 čísly oddělenými mezerou. Např. 0 0, 100 0, 90, 67. Nepbo procenty, např. 2% 2%
+// color-stop   -
+// URI          - příklad:  .image-view { -fx-image: url("http://jdojo.com/myimage.png"); }
+// effect       - příklad:  .drop-shadow { -fx-effect: dropshadow(gaussian, gray, 10, 0.6, 10, 10); }
+// font         - skládá se ze 4 atributů: family, size, style, wight. Je možné buď
+//                    - specifikovat atributy jednotlivě za použití 4 CSS vlastností:
+//                      -fx-font-family, -fx-font-size, -fx-font-style, -fx-font-weight, př.:
+//                      .my-font-style {
+//                          -fx-font-family: "serif";
+//                          -fx-font-size: 20px;
+//                          -fx-font-style: normal;
+//                          -fx-font-weight: bolder;
+//                      }
+//                    - za použití 1 CSS vlastnosti specifikovat vše:
+//                      -fx-font: <font-style> <font-weight> <font-size> <font-family>
+//                      .my-font-style { -fx-font: italic bolder 20px "serif" }
+// paint        - specifikuje barvu, nebo vzory výplně. Např. pro obdelníkové uzly, či pro pozadí tlačíek.
+
+public class FCSSPropertiesTypes {
+}
