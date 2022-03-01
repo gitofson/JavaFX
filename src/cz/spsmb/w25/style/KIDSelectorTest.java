afe86b3e56d40144df73dfc0e0060836d07bfbd3
stemb <stemberk@spsmb.cz>
2022-03-01 19:47:30 +0100

    KIDSelectorTest

--- src/cz/spsmb/w25/style/KIDSelectorTest.java
+++ src/cz/spsmb/w25/style/KIDSelectorTest.java
@@ -1,5 +1,12 @@
 package cz.spsmb.w25.style;
+// Třída uzel má k dispozici vlastnost id (typ StringProperty), která může být použita k přiřazení jednoznačného id ke
+// každému uzlu. V style sheetu je pak tyto selektory namísto tečky prefixovat mřížkou:
+// #closeButton {
+//  	-fx-text-fill: red;
+// }
 
+// JavaFX používá tzv. specifikaci selektoru (obdoba priority), která určí jaký selector bude ve finále použit. ID selector
+// má v příkladu větší specifikaci, než class selector a proto je u closeBtn použit.
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,6 +23,7 @@ public class KIDSelectorTest extends Application {
         Button openBtn = new Button("Open");
         Button saveBtn = new Button("Save");
 
+        // nastavení ID closeBtn
         Button closeBtn = new Button("Close");
         closeBtn.setId("closeButton");
 
