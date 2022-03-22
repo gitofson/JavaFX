ced660f94747ea2f36951549de64bcdd2a33251a
stemb <stemberk@spsmb.cz>
2022-03-22 21:34:24 +0100

    LDomainObjectsInChoiceBox

--- src/cz/spsmb/w28/control/LDomainObjectsInChoiceBox.java
+++ src/cz/spsmb/w28/control/LDomainObjectsInChoiceBox.java
@@ -14,19 +14,23 @@ import javafx.stage.Stage;
 
 // V ChoiceBoxu nemusí být nutně jen položky typu String. Lze použít jakýkoliv objekt. ChoiceBox volá metodu toString()
 // každé položky a zobrazí ji v pop-up menu. Třída
-// cz.spsmb.w28.PersonStringConverter disponuje matodami toString() a fromString(String in). Více v příkladu.
+// cz.spsmb.w28.PersonStringConverter disponuje matodami toString() a fromString(String in). Více v příkladu, kde jsou
+// na ukázku přidány i styly.
+
 public class LDomainObjectsInChoiceBox extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         ChoiceBox<Person> persons = new ChoiceBox<>();
         //Nastavení konvertoru pro konverzi objektu Person na String
         //persons.setConverter(new PersonStringConverter());
+
         persons.getItems().addAll(new Person("John", "Jacobs", null),
                                   new Person("Donna", "Duncan", null),
                                   new Person("Layne", "Estes", null),
                                   new Person("Mason", "Boyd", null));
         HBox root = new HBox(persons);
         Scene scene = new Scene(root);
+        scene.getStylesheets().add("css/choice_box.css");
         stage.setScene(scene);
         stage.show();
     }
